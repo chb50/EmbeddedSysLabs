0084b70a80ccf56072fec05f5d5d93c0ecbfd8d1
Cedric Blake <Cedric Blake>
2016-09-14 14:38:04 -0400

    Finished all of lab 1

--- FPGA lab/lab1/lab1.vhd
+++ FPGA lab/lab1/lab1.vhd
@@ -1,23 +1,24 @@
---library ieee;
---use ieee.std_logic_1164.all;
---
-----MAKE SURE TO ADD TIMINGS
---
-----Begin entity
---entity lab1 is
---port (sw: in std_logic_vector(2 downto 1);
---		  LEDR: out std_logic_vector(0 downto 0));
---end entity lab1;
---
-----begin behavior
---architecture behavior of lab1 is
-----member variable
---signal sig1, sig2: std_logic;
---constant gate_delay: time:= 5ns;
-----not assigning gate delays yet
-----begin function
---begin
---	sig1 <= (not (sw(1) or not sw(2))) after gate_delay;
---	sig2 <= (not sw(1) and sw(2)) after gate_delay;
---	LEDR(0) <= (not (sig1 and sig2)) after gate_delay;
---end architecture behavior;
+library ieee;
+use ieee.std_logic_1164.all;
+
+--MAKE SURE TO ADD TIMINGS
+
+--Begin entity
+
+entity lab1 is
+port (sw: in std_logic_vector(2 downto 1);
+		  LEDR: out std_logic_vector(0 downto 0));
+end entity lab1;
+
+--begin behavior
+architecture behavior of lab1 is
+--member variable
+signal sig1, sig2: std_logic;
+constant gate_delay: time:= 5ns;
+--not assigning gate delays yet
+--begin function
+begin
+	sig1 <= (not (sw(1) or not sw(2))) after gate_delay;
+	sig2 <= (not sw(1) and sw(2)) after gate_delay;
+	LEDR(0) <= (not (sig1 and sig2)) after gate_delay;
+end architecture behavior;
--- FPGA lab/lab1sevenseg/lab1sevenseg.vhd
+++ FPGA lab/lab1sevenseg/lab1sevenseg.vhd
@@ -3,7 +3,7 @@ use ieee.std_logic_1164.all;
 
 entity lab1sevenseg is
 port (SW: in std_logic_vector(3 downto 0);
-		HEX: out std_logic_vector(6 downto 0));
+		HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, HEX6, HEX7: out std_logic_vector(6 downto 0));
 end entity lab1sevenseg;
 
 architecture behavior of lab1sevenseg is
@@ -28,7 +28,140 @@ signal error: std_logic_vector(6 downto 0):= "0111111";
 constant gate_delay: time:=5ns;
 begin
 with SW select
-HEX <= zero after gate_delay when "0000", --0
+HEX0 <= zero after gate_delay when "0000", --0
+			one after gate_delay when "0001", --1
+			two after gate_delay when "0010", --2
+			three after gate_delay when "0011", --3
+			four after gate_delay when "0100", --4
+			five after gate_delay when "0101", --5
+			six after gate_delay when "0110", --6
+			seven after gate_delay when "0111", --7
+			eight after gate_delay when "1000", --8
+			nine after gate_delay when "1001", --9
+			a after gate_delay when "1010", --a
+			b after gate_delay when "1011", --b
+			c after gate_delay when "1100", --c
+			d after gate_delay when "1101", --d
+			e after gate_delay when "1110", --e
+			f after gate_delay when "1111", --f
+			error after gate_delay when others; -- gives a "-" sign when an error has occured
+			
+with SW select
+HEX1 <= zero after gate_delay when "0000", --0
+			one after gate_delay when "0001", --1
+			two after gate_delay when "0010", --2
+			three after gate_delay when "0011", --3
+			four after gate_delay when "0100", --4
+			five after gate_delay when "0101", --5
+			six after gate_delay when "0110", --6
+			seven after gate_delay when "0111", --7
+			eight after gate_delay when "1000", --8
+			nine after gate_delay when "1001", --9
+			a after gate_delay when "1010", --a
+			b after gate_delay when "1011", --b
+			c after gate_delay when "1100", --c
+			d after gate_delay when "1101", --d
+			e after gate_delay when "1110", --e
+			f after gate_delay when "1111", --f
+			error after gate_delay when others; -- gives a "-" sign when an error has occured
+
+with SW select
+HEX2 <= zero after gate_delay when "0000", --0
+			one after gate_delay when "0001", --1
+			two after gate_delay when "0010", --2
+			three after gate_delay when "0011", --3
+			four after gate_delay when "0100", --4
+			five after gate_delay when "0101", --5
+			six after gate_delay when "0110", --6
+			seven after gate_delay when "0111", --7
+			eight after gate_delay when "1000", --8
+			nine after gate_delay when "1001", --9
+			a after gate_delay when "1010", --a
+			b after gate_delay when "1011", --b
+			c after gate_delay when "1100", --c
+			d after gate_delay when "1101", --d
+			e after gate_delay when "1110", --e
+			f after gate_delay when "1111", --f
+			error after gate_delay when others; -- gives a "-" sign when an error has occured
+
+with SW select
+HEX3 <= zero after gate_delay when "0000", --0
+			one after gate_delay when "0001", --1
+			two after gate_delay when "0010", --2
+			three after gate_delay when "0011", --3
+			four after gate_delay when "0100", --4
+			five after gate_delay when "0101", --5
+			six after gate_delay when "0110", --6
+			seven after gate_delay when "0111", --7
+			eight after gate_delay when "1000", --8
+			nine after gate_delay when "1001", --9
+			a after gate_delay when "1010", --a
+			b after gate_delay when "1011", --b
+			c after gate_delay when "1100", --c
+			d after gate_delay when "1101", --d
+			e after gate_delay when "1110", --e
+			f after gate_delay when "1111", --f
+			error after gate_delay when others; -- gives a "-" sign when an error has occured
+
+with SW select
+HEX4 <= zero after gate_delay when "0000", --0
+			one after gate_delay when "0001", --1
+			two after gate_delay when "0010", --2
+			three after gate_delay when "0011", --3
+			four after gate_delay when "0100", --4
+			five after gate_delay when "0101", --5
+			six after gate_delay when "0110", --6
+			seven after gate_delay when "0111", --7
+			eight after gate_delay when "1000", --8
+			nine after gate_delay when "1001", --9
+			a after gate_delay when "1010", --a
+			b after gate_delay when "1011", --b
+			c after gate_delay when "1100", --c
+			d after gate_delay when "1101", --d
+			e after gate_delay when "1110", --e
+			f after gate_delay when "1111", --f
+			error after gate_delay when others; -- gives a "-" sign when an error has occured
+			
+with SW select
+HEX5 <= zero after gate_delay when "0000", --0
+			one after gate_delay when "0001", --1
+			two after gate_delay when "0010", --2
+			three after gate_delay when "0011", --3
+			four after gate_delay when "0100", --4
+			five after gate_delay when "0101", --5
+			six after gate_delay when "0110", --6
+			seven after gate_delay when "0111", --7
+			eight after gate_delay when "1000", --8
+			nine after gate_delay when "1001", --9
+			a after gate_delay when "1010", --a
+			b after gate_delay when "1011", --b
+			c after gate_delay when "1100", --c
+			d after gate_delay when "1101", --d
+			e after gate_delay when "1110", --e
+			f after gate_delay when "1111", --f
+			error after gate_delay when others; -- gives a "-" sign when an error has occured
+			
+with SW select
+HEX6 <= zero after gate_delay when "0000", --0
+			one after gate_delay when "0001", --1
+			two after gate_delay when "0010", --2
+			three after gate_delay when "0011", --3
+			four after gate_delay when "0100", --4
+			five after gate_delay when "0101", --5
+			six after gate_delay when "0110", --6
+			seven after gate_delay when "0111", --7
+			eight after gate_delay when "1000", --8
+			nine after gate_delay when "1001", --9
+			a after gate_delay when "1010", --a
+			b after gate_delay when "1011", --b
+			c after gate_delay when "1100", --c
+			d after gate_delay when "1101", --d
+			e after gate_delay when "1110", --e
+			f after gate_delay when "1111", --f
+			error after gate_delay when others; -- gives a "-" sign when an error has occured
+			
+with SW select
+HEX7 <= zero after gate_delay when "0000", --0
 			one after gate_delay when "0001", --1
 			two after gate_delay when "0010", --2
 			three after gate_delay when "0011", --3
